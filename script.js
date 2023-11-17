8536429bcdbb10e003b85cf039d8e837c2342d25
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-11-16 21:14:30 -0500

    Finish working console version. Start screen code.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Gameboard() {
+function Gameboard () {
   const rows = 3;
   const columns = 3;
   const board = [];
@@ -7,38 +7,43 @@ function Gameboard() {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
       board[i].push(Cell());
-    }; 
+    };
   };
-
   const getBoard = () => board;
 
-  const dropToken = (row, column, player) => {
-  board[row][column].addToken(player);
+  const setToken = (row, column, player) => {
+    if (board[row][column] !== 1||2) {
+      board[row][column].addToken(player);
+    } else {
+      alert('Space already taken.')
+      return;
+    }
   };
   const printBoard = () => {
-  const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
-  console.log(boardWithCellValues);
-  }
-  return {getBoard, dropToken, printBoard};
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  };
+  return {getBoard, setToken, printBoard}
 };
-
-
 
 function Cell() {
   let value = 0;
+
   const addToken = (player) => {
     value = player;
   };
+
   const getValue = () => value;
+
   return {
     addToken,
     getValue
   };
-};
+}
 
-function GameController(
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
+function GameController (
+  playerOneName = 'Player One',
+  playerTwoName = 'Player Two'
 ) {
   const board = Gameboard();
 
@@ -66,19 +71,16 @@ function GameController(
   };
 
   const playRound = (row, column) => {
-    // Drop a token for the current player
     console.log(
       `Placing ${getActivePlayer().name}'s token into row ${row}, column ${column}...`
     );
-    board.dropToken(row, column, getActivePlayer().token);
-
-    // Switch player turn
-    switchPlayerTurn();
-    printNewRound();
-  };
-
-  // Initial play game message
+    board.setToken(row, column, getActivePlayer().token);
+  switchPlayerTurn();
   printNewRound();
+  };
+  
+  printNewRound();
+  
 
   return {
     playRound,
@@ -86,4 +88,52 @@ function GameController(
   };
 }
 
-const game = GameController();
+function ScreenController() {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector('.turn');
+  const boardDiv = document.querySelector('.board');
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+    // Render board squares
+    board.forEach(row => {
+      row.forEach((cell, index) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function 
+        cellButton.dataset.column = index
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      })
+    })
+  }
+
+  // Add event listener for the board
+  function clickHandlerBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn) return;
+    
+    game.playRound(selectedColumn);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  // Initial render
+  updateScreen();
+
+  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+
+ScreenController();
